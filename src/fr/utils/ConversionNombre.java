fb4dbf15d26f61a57faaeb6cc9b98afac799fbb6
StephanieMC <StephanieMC@StephMC.home>
2021-01-27 15:52:10 +0100

    4th commint

---
+++ src/fr/utils/ConversionNombre.java
@@ -0,0 +1,22 @@
+package fr.utils;
+
+import java.lang.Integer;
+
+
+public class ConversionNombre {
+	public static void main(String[] args) {
+		
+		String chaine ="12";
+		
+		int chaine1 = Integer.parseInt(chaine);
+		
+		int a = 5;
+		int b = 10;
+		
+		int maxValue = Integer.max(a,b);
+		
+		System.out.println(maxValue);
+		
+	}
+		
+}
