3fd56200fc84de9ef20c94a001b56e01c6a3e445
StephanieMC <StephanieMC@StephMC.home>
2021-02-01 10:25:58 +0100

    TP exercice calcul moyenne

---
+++ src/fr/diginamic/operations/CalculMoyenne.java
@@ -0,0 +1,49 @@
+package fr.diginamic.operations;
+
+public class CalculMoyenne {
+
+	double[] tab = new double[2];
+	int i = 0;
+
+	public CalculMoyenne() { // constructeur
+		super();
+	}
+
+	public void ajout(double nb) {
+
+		if (i > tab.length - 1) {
+
+			double[] tab1 = new double[tab.length + 1];
+
+			for (int j = 0; j < tab.length; j++) {
+
+				tab1[j] = tab[i];
+
+			}
+
+			tab = tab1;
+
+		}
+
+		tab[i] = nb;
+
+		i++;
+
+	}
+
+	public double calcul() { 
+		
+		double total = 0;
+		
+		for( int i = 0; i < tab.length; i ++) {
+			
+			total += tab[i];
+		}
+		
+		double moyenne = total / tab.length;
+		
+		return moyenne;
+
+	}
+
+}
